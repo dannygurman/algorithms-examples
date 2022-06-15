232d6ffabf9a663aae9359815687dea7e04b8f1c
Danny Gurman <danny.gurman@algosec.com>
2022-06-15 15:52:34 +0300

    Trie 4

---
+++ algorithms-examples/src/main/java/algorithms/trie/trie2/TrieNodeV2.java
@@ -0,0 +1,9 @@
+package algorithms.trie.trie2;
+
+public class TrieNodeV2 {
+    static int ALPHABET_SIZE = 26;
+
+    TrieNodeV2 children[] = new TrieNodeV2[ALPHABET_SIZE];
+    boolean isEndOfWord;
+
+}
---
+++ algorithms-examples/src/main/java/algorithms/trie/trie2/TrieV2.java
@@ -0,0 +1,101 @@
+package algorithms.trie.trie2;
+
+import algorithms.trie.trie1.TrieNodeV1;
+
+public class TrieV2 {
+
+    private TrieNodeV2 root;
+
+    public TrieV2() {
+        root = new TrieNodeV2();
+    }
+
+    private static int getCharIndex(char char1) {
+      return char1 - 'a';
+    }
+
+    // If not present, inserts key into trie
+    // If the key is prefix of trie node, just marks leaf node
+        public void insert(String word) {
+        TrieNodeV2 current = root;
+
+        for (int i = 0; i < word.length(); i++) {
+            int index = getCharIndex(word.charAt(i));
+            TrieNodeV2 childNodeAtIndex = current.children[index];
+            if (childNodeAtIndex == null) {
+                childNodeAtIndex = new TrieNodeV2();
+            }
+            current = childNodeAtIndex;
+        }
+
+        // mark last node as leaf
+        current.isEndOfWord = true;
+    }
+
+    // Returns true if key presents in trie, else
+    // false
+    static boolean search(TrieNode root, String key) {
+        TrieNode pCrawl = root;
+
+        for (int i = 0; i < key.length(); i++) {
+            int index = key.charAt(i) - 'a';
+            if (pCrawl.children[index] == null) {
+                return false;
+            }
+
+            pCrawl = pCrawl.children[index];
+        }
+
+        return (pCrawl != null && pCrawl.isEndOfWord);
+    }
+
+    // Returns true if root has no children, else false
+    static boolean isEmpty(TrieNode root) {
+        for (int i = 0; i < ALPHABET_SIZE; i++) {
+            if (root.children[i] != null) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // Recursive function to delete a key from given Trie
+    static TrieNode remove(TrieNode root, String key, int depth) {
+        // If tree is empty
+        if (root == null) {
+            return null;
+        }
+
+        // If last character of key is being processed
+        if (depth == key.length()) {
+
+            // This node is no more end of word after
+            // removal of given key
+            if (root.isEndOfWord) {
+                root.isEndOfWord = false;
+            }
+
+            // If given is not prefix of any other word
+            if (isEmpty(root)) {
+                root = null;
+            }
+
+            return root;
+        }
+
+        // If not last character, recur for the child
+        // obtained using ASCII value
+        int index = key.charAt(depth) - 'a';
+        root.children[index] =
+            remove(root.children[index], key, depth + 1);
+
+        // If root does not have any child (its only child got
+        // deleted), and it is not end of another word.
+        if (isEmpty(root) && root.isEndOfWord == false) {
+            root = null;
+        }
+
+        return root;
+    }
+
+}
