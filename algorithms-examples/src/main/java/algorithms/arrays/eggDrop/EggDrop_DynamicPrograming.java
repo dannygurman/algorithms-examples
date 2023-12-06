31109e4354532c956844f52406f70c0181c24179
Danny Gurman <danny.gurman@algosec.com>
2023-12-06 09:31:12 +0200

    eggDrop4

--- algorithms-examples/src/main/java/algorithms/arrays/eggDrop/EggDrop_DynamicPrograming.java
+++ algorithms-examples/src/main/java/algorithms/arrays/eggDrop/EggDrop_DynamicPrograming.java
@@ -4,20 +4,18 @@ package algorithms.arrays.eggDrop;
 //Time Complexity: O(n_eggs * k_floors^2)
 //Auxiliary Space: O(n_eggs * k_floors)
 class EggDrop_DynamicPrograming {
-    // A utility function to get maximum of two integers
-    static int max(int a, int b) { return (a > b) ? a : b; }
 
     /* Function to get minimum number of trials needed in worst
     case with n eggs and k floors */
     static int eggDrop(int n_eggs, int k_floors)
     {
-		/* A 2D table where entery eggFloor[i][j] will represent minimum
-	number of trials needed for i eggs and j floors. */
+		/* A 2D table where entry eggFloor[i][j] will represent minimum
+			number of trials needed for i eggs and j floors. */
         int eggFloor[][] = new int[n_eggs + 1][k_floors + 1];
         int res;
         int egg_count, floor_id, tmp_floor_id;
 
-        // We need one trial for one floor and0 trials for 0 floors
+        // We need one trial for one floor and 0 trials for 0 floors
         for (egg_count = 1; egg_count <= n_eggs; egg_count++) {
             eggFloor[egg_count][1] = 1;
             eggFloor[egg_count][0] = 0;
@@ -29,9 +27,7 @@ class EggDrop_DynamicPrograming {
         }
 
 
-
-        // Fill rest of the entries in table using optimal substructure
-        // property
+        // Fill rest of the entries in table using optimal substructure property
         for (egg_count = 2; egg_count <= n_eggs; egg_count++) {
             for (floor_id = 2; floor_id <= k_floors; floor_id++) {
                //First - init to max
@@ -46,11 +42,9 @@ class EggDrop_DynamicPrograming {
                     //The floors we want to test aren't important;
                     //in fact, the number of remaining floors is what matters.
                     //For example, testing the floors between 1 and 20 (both 1 and 20 included)
-                    //would require the same number
-                    // of drops to test the floors between 21 and 40
+                    //would require the same number of drops to test the floors between 21 and 40
                     //(after knowing that we can drop from floor 20 and the egg will not be broken)
-                    //, or between 81 and 100. In all
-                    // three situations, we tested 20 floors
+                    //, or between 81 and 100. In all three situations, we tested 20 floors
 
                     //the range [tmp_floor_id -> floor_id]
                     //is like [(tmp_floor_id - tmp_floor_id) -> (floor_id - tmp_floor_id)]
@@ -58,7 +52,7 @@ class EggDrop_DynamicPrograming {
                     int min_trails_upper_flower =  eggFloor[egg_count][floor_id - tmp_floor_id];
 
                     //Adding '1'  - for this trial
-                    res = 1 + max(min_trails_lower_flower_minus_egg, min_trails_upper_flower);
+                    res = 1 + Math.max(min_trails_lower_flower_minus_egg, min_trails_upper_flower);
 
                     if (res < eggFloor[egg_count][floor_id])
                         eggFloor[egg_count][floor_id] = res;
@@ -70,12 +64,11 @@ class EggDrop_DynamicPrograming {
         return eggFloor[n_eggs][k_floors];
     }
 
-    /* Driver program to test to pront printDups*/
-    public static void main(String args[])
-    {
+    /* Driver program to test */
+    public static void main(String args[])  {
         int n_eggs = 2, k_floors = 100;
-        System.out.println("Minimum number of trials in "+
-                "worst case with "
-                + n_eggs+ " eggs and " + k_floors + " floors is " + eggDrop(n_eggs, k_floors));
+        int miNumOfTrials = eggDrop(n_eggs, k_floors);
+        System.out.println("Minimum number of trials in worst case with "
+                + n_eggs+ " eggs and " + k_floors + " floors is " + miNumOfTrials);
     }
 }
