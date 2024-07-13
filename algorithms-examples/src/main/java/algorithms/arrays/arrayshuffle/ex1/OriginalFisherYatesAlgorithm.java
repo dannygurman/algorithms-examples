42d5d8f6228332a50d2d8e0716cc362da93f031d
dannygurman <danny.gurman@gmail.com>
2024-07-13 18:47:20 +0300

    shuffle

--- algorithms-examples/src/main/java/algorithms/arrays/arrayshuffle/ex1/OriginalFisherYatesAlgorithm.java
+++ algorithms-examples/src/main/java/algorithms/arrays/arrayshuffle/ex1/OriginalFisherYatesAlgorithm.java
@@ -7,16 +7,18 @@ import java.util.Random;
 public class OriginalFisherYatesAlgorithm {
 
     // Function to perform Fisher-Yates Shuffle using the original method
-    public static List<Character> fisherYatesShuffle(List<Character> arr) {
+    public static List<Character> fisherYatesShuffle(List<Character> list) {
         List<Character> output = new ArrayList<>(); // Create an output list to store the shuffled elements
-        boolean[] visited = new boolean[arr.size()]; // Create a boolean array to keep track of visited elements
+        boolean[] visited = new boolean[list.size()]; // Create a boolean array to keep track of visited elements
+
         // Perform the shuffle algorithms
-        for (int i = 0; i < arr.size(); i++) {
-            int j = generateRandomIndexInList(arr); // Generate a random index in the input list
+        for (int i = 0; i < list.size(); i++) {
+            int j = generateRandomIndexInList(list); // Generate a random index in the input list
             while (visited[j]) { // Find the next unvisited index
-                j = generateRandomIndexInList(arr);
+                j = generateRandomIndexInList(list);
             }
-            output.add(arr.get(j)); // Add the element at the chosen index to the output list
+
+            output.add(list.get(j)); // Add the element at the chosen index to the output list
             visited[j] = true; // Mark the element as visited
         }
         return output;
